5bed214bc49b8690937998af4ab06d0815b7508c
이해용 <evilgon@nate.com>
2023-05-08 17:44:15 +0900

    fusionChart.xml 최초 커밋

---
+++ src/main/webapp/websquare/config.js
@@ -0,0 +1,615 @@
+﻿﻿﻿﻿export default {
+  "WebSquare": {
+    "convertPageXML": {
+      "@value": "true"
+    },
+    "wpack": {
+      "@use": "true",
+      "contextRoot": {
+        "@value": "/"
+      },
+      "srcExtension": {
+        "@value": "xml"
+      },
+      "destExtension": {
+        "@value": "js"
+      },
+      "destRoot": {
+        "@value": "_wpack_"
+      }
+    },
+    "allValue": {
+      "@value": "all"
+    },
+    "docType": {
+      "@value": "standard"
+    },
+    "debug": {
+      "@remoteConsole": "false",
+      "@value": "true"
+    },
+    "debugKey": {
+      "@value": ""
+    },
+    "language": {
+      "@value": "ko"
+    },
+    "useItemLocale": {
+      "@value": "false"
+    },
+    "date": {},
+    "exceptionHandler": {
+      "@value": "websquare"
+    },
+    "debugMenu": {
+      "@value": "use"
+    },
+    "welcome-file": {},
+    "postDrawMode": {
+      "@value": "synchronous"
+    },
+    "forcedValueSetting": {
+      "@value": "true"
+    },
+    "processMsgHeight": {
+      "@value": "81"
+    },
+    "processMsgWidth": {
+      "@value": "295"
+    },
+    "processMsgBackground": {
+      "@backgroundColor": "#112233",
+      "@value": "true"
+    },
+    "byteCheckEncoding": {
+      "@value": "euc-kr"
+    },
+    "initScript": {
+      "@value": "false"
+    },
+    "clearMemory": {
+      "@value": "false"
+    },
+    "stylesheet": {
+      "@earlyImportList": "/cm/css/base.css",
+      "@enable": "true",
+      "@import": "link",
+      "@value": "stylesheet_ext.css"
+    },
+    "mediaInfo": {
+      "media": [
+        {
+          "@maxWidth": "",
+          "@minWidth": "1025",
+          "@name": "desktop"
+        },
+        {
+          "@maxWidth": "1024",
+          "@minWidth": "768",
+          "@name": "tablet"
+        },
+        {
+          "@maxWidth": "767",
+          "@minWidth": "320",
+          "@name": "mobile"
+        }
+      ]
+    },
+    "style": {
+      "removeDefaultClass": {
+        "@value": "true"
+      }
+    },
+    "engine": {},
+    "ModelUtil": {
+      "copyChildrenNodes": {
+        "@refresh": "false"
+      }
+    },
+    "preProcessor": {
+      "systemPreProcessor": {
+        "@value": ""
+      },
+      "businessPreProcessor": {
+        "@value": ""
+      }
+    },
+    "workflow": {
+      "processMsg": {
+        "@value": ""
+      },
+      "makeGlobalObject": {
+        "@value": "true"
+      }
+    },
+    "submission": {
+      "processMsg": {
+        "@value": ""
+      },
+      "showSubmissionTime": {
+        "@value": "true"
+      },
+      "preSubmitFunction": {
+        "@value": "presubmit"
+      },
+      "callbackSubmitFunction": {
+        "@value": ""
+      },
+      "requestID": {
+        "@value": ""
+      },
+      "makeGlobalObject": {
+        "@value": "true"
+      }
+    },
+    "visibleHelper": {
+      "targetHandler": {
+        "@value": ""
+      }
+    },
+    "spa": {
+      "onpageunload": {
+        "@value": ""
+      },
+      "variable": {
+        "@value": "scwin"
+      },
+      "scriptCache": {
+        "@value": "true"
+      },
+      "autoReload": {
+        "@count": "50",
+        "@value": "true"
+      }
+    },
+    "scriptLoading": {
+      "@merge": "true"
+    },
+    "scriptPrecedence": {
+      "@value": "true"
+    },
+    "strictMode": {
+      "@id": "mf",
+      "@value": "true"
+    },
+    "engineCache": {
+      "@compression": "true",
+      "@enable": "true",
+      "@postfix": "month"
+    },
+    "userAgentPattern": {
+      "@XPathParser": "Edge"
+    },
+    "preserveWhiteSpace": {
+      "@value": "false"
+    },
+    "environment": {
+      "@cache": "nocache",
+      "@mode": "production",
+      "@postfix": "day"
+    },
+    "script": {
+      "@postfix": "environment"
+    },
+    "emptyTag": {
+      "@value": "area,base,basefont,br,col,frame,hr,img,input,link,meta,param"
+    },
+    "engineLoadingMode": {
+      "@android": "0",
+      "@chrome": "0",
+      "@ie": "0",
+      "@iphone": "0",
+      "@moz": "0",
+      "@opera": "0",
+      "@safari": "0"
+    },
+    "engineChunkNum": {
+      "@value": "1"
+    },
+    "dataList": {
+      "removeDummyRowStatus": {
+        "@value": "true"
+      },
+      "removedDataMatch": {
+        "@value": "true"
+      },
+      "saveRemovedDataDeletedInsertedRow": {
+        "@value": "false"
+      }
+    },
+    "grid": {
+      "rowNumStatusUniqueId": {
+        "@value": "true"
+      },
+      "drilldownRealRowIndexAll": {
+        "@value": "true"
+      },
+      "collectGarbage": {
+        "@value": "none"
+      },
+      "fastScroll": {
+        "@value": "true"
+      },
+      "dataType": {
+        "date": {
+          "@displayFormat": "yyyy-MM-dd"
+        }
+      },
+      "editType": {
+        "@value": "focus"
+      },
+      "rowNumBackgroundColor": {
+        "@value": "#e5eff7"
+      },
+      "selectedRowColor": {
+        "@value": "#fcf8e3"
+      },
+      "oddEvenColorDisplay": {
+        "@value": "true"
+      },
+      "evenRowBackgroundColor": {
+        "@value": "#f5f5f5"
+      },
+      "oddRowBackgroundColor": {
+        "@value": "#ffffff"
+      },
+      "rowMouseOver": {
+        "@value": "true"
+      },
+      "rowMouseOverColor": {
+        "@value": "#edf3fb"
+      },
+      "tooltipStyle": {
+        "@value": "padding:1px 3px 0;line-height:14px;font-size:12px;border:0;background-color:#5c85d4;color:#fff"
+      },
+      "noResultMessageVisible": {
+        "@value": "true"
+      },
+      "noResultMessage": {
+        "@value": "데이터가 없음"
+      },
+      "noResultMessageStyle": {
+        "@value": "position:absolute; left:40%; width:20%; top:40%; border:1px solid #b3b3b3; color:#383d41; font-size:12px; padding:5px; text-align:center; background:#fafafa"
+      },
+      "pasteMessage": {
+        "@value": "데이터 붙이는 중"
+      },
+      "getColumnVisible": {
+        "@useRealColIndex": "true"
+      },
+      "checkDisabledOnPaste": {
+        "@value": "true"
+      },
+      "checkReadOnlyOnPaste": {
+        "@value": "true"
+      },
+      "colIdToColIndex": {
+        "@value": "true"
+      },
+      "column": [
+        {
+          "@inputType": "text",
+          "editType": {
+            "@value": "select"
+          }
+        },
+        {
+          "@inputType": "select",
+          "chooseOptionLabel": {
+            "@value": "-선택-"
+          }
+        },
+        {
+          "@inputType": "calendar",
+          "dataType": {
+            "@value": "date"
+          },
+          "displayFormat": [
+            {
+              "@value": "yyyy-MM",
+              "@valueType": "yearMonth"
+            },
+            {
+              "@value": "yyyy-MM-dd",
+              "@valueType": "yearMonthDate"
+            },
+            {
+              "@value": "yyyy-MM-dd HH:mm",
+              "@valueType": "yearMonthDateTime"
+            },
+            {
+              "@value": "yyyy-MM-dd HH:mm:SS",
+              "@valueType": "yearMonthDateTimeSec"
+            }
+          ]
+        }
+      ]
+    },
+    "gridView": {
+      "drilldownFooterExpressionAllData": {
+        "@value": "true"
+      },
+      "rowNumStatusUniqueId": {
+        "@value": "true"
+      },
+      "preventMultipleClick": {
+        "@value": "true"
+      },
+      "drilldownRealRowIndexAll": {
+        "@value": "true"
+      },
+      "collectGarbage": {
+        "@value": "none"
+      },
+      "fastScroll": {
+        "@value": "true"
+      },
+      "dataType": {
+        "date": {
+          "@displayFormat": "yyyy-MM-dd"
+        }
+      },
+      "editType": {
+        "@value": "focus"
+      },
+      "rowNumBackgroundColor": {
+        "@value": "#e5eff7"
+      },
+      "selectedRowColor": {
+        "@value": "#fcf8e3"
+      },
+      "oddEvenColorDisplay": {
+        "@value": "true"
+      },
+      "evenRowBackgroundColor": {
+        "@value": "#f5f5f5"
+      },
+      "oddRowBackgroundColor": {
+        "@value": "#ffffff"
+      },
+      "rowMouseOver": {
+        "@value": "true"
+      },
+      "rowMouseOverColor": {
+        "@value": "#edf3fb"
+      },
+      "tooltipStyle": {
+        "@value": "padding:1px 3px 0;line-height:14px;font-size:12px;border:0;background-color:#5c85d4;color:#fff"
+      },
+      "noResultMessageVisible": {
+        "@value": "true"
+      },
+      "noResultMessage": {
+        "@value": "데이터가 없음"
+      },
+      "noResultMessageStyle": {
+        "@value": "position:absolute; left:40%; width:20%; top:40%; border:1px solid #b3b3b3; color:#383d41; font-size:12px; padding:5px; text-align:center; background:#fafafa"
+      },
+      "pasteMessage": {
+        "@value": "데이터 붙이는 중"
+      },
+      "getColumnVisible": {
+        "@useRealColIndex": "true"
+      },
+      "checkDisabledOnPaste": {
+        "@value": "true"
+      },
+      "checkReadOnlyOnPaste": {
+        "@value": "true"
+      },
+      "colIdToColIndex": {
+        "@value": "true"
+      },
+      "column": [
+        {
+          "@inputType": "text",
+          "editType": {
+            "@value": "select"
+          }
+        },
+        {
+          "@inputType": "select",
+          "chooseOptionLabel": {
+            "@value": "-선택-"
+          },
+          "eventPriority": {
+            "@value": "oneditend"
+          }
+        },
+        {
+          "@inputType": "calendar",
+          "dataType": {
+            "@value": "date"
+          },
+          "displayFormat": [
+            {
+              "@value": "yyyy-MM",
+              "@valueType": "yearMonth"
+            },
+            {
+              "@value": "yyyy-MM-dd",
+              "@valueType": "yearMonthDate"
+            },
+            {
+              "@value": "yyyy-MM-dd HH:mm",
+              "@valueType": "yearMonthDateTime"
+            },
+            {
+              "@value": "yyyy-MM-dd HH:mm:SS",
+              "@valueType": "yearMonthDateTimeSec"
+            },
+            {
+              "@value": "yyyy-MM-dd HH:mm:SS",
+              "@valueType": "yearMonthDateTimeSec"
+            }
+          ]
+        }
+      ]
+    },
+    "inputCalendar": {
+      "validCheck": {
+        "@value": "false"
+      },
+      "dataType": {
+        "@value": "date"
+      },
+      "delimiter": {
+        "@value": "-"
+      },
+      "mask": {
+        "@value": "MM-dd-yyyy"
+      },
+      "delimiterLocaleKey": {
+        "@value": "IC_Delimiter"
+      },
+      "maskLocaleKey": {
+        "@value": "IC_Mask"
+      },
+      "calendarImage": {
+        "@value": ""
+      },
+      "calendarImageOver": {
+        "@value": ""
+      }
+    },
+    "input": {
+      "focusStyle": {
+        "@value": ""
+      },
+      "dateMask": {
+        "@value": "yyyy-MM-dd"
+      },
+      "timeMask": {
+        "@value": "HH:mm"
+      },
+      "focusCalcSize": {
+        "@value": "false"
+      }
+    },
+    "calendar": {
+      "minYear": {
+        "@value": "1978"
+      },
+      "maxYear": {
+        "@value": "2030"
+      }
+    },
+    "selectbox": {
+      "visibleRowNum": {
+        "@value": "5"
+      },
+      "dataListAutoRefresh": {
+        "@value": "true"
+      }
+    },
+    "tabControl": {},
+    "treeview": {
+      "tooltipGroupClass": {
+        "@value": "false"
+      }
+    },
+    "trigger": {
+      "preventMultipleClick": {
+        "@value": "true"
+      }
+    },
+    "anchor": {
+      "preventMultipleClick": {
+        "@value": "true"
+      }
+    },
+    "wframe": {
+      "mode": {
+        "@value": "sync"
+      },
+      "scope": {
+        "@value": "true"
+      }
+    },
+    "pageInherit": {
+      "mode": {
+        "@value": "sync"
+      }
+    },
+    "windowContainer": {
+      "tooltipGroupClass": {
+        "@value": "false"
+      },
+      "getWindow": {
+        "@searchTarget": "windowId"
+      },
+      "displayOnlyTopWindow": {
+        "@value": "true"
+      }
+    },
+    "pageList": {},
+    "radio": {
+      "nameScope": {
+        "@value": "true"
+      }
+    },
+    "body": {
+      "toolTipSec": {
+        "@value": "1"
+      }
+    },
+    "editor": {
+      "version": {
+        "@value": "4.11.3"
+      }
+    },
+    "fusionchart": {},
+    "languagePack": {
+      "@useLanguagePack": "false",
+      "url": [
+        {
+          "@lang": "ko",
+          "@value": "/langpack/ko.js"
+        },
+        {
+          "@lang": "en",
+          "@value": "/langpack/en.js"
+        },
+        {
+          "@lang": "ch",
+          "@value": "/langpack/ch.js"
+        }
+      ]
+    },
+    "projectCommon": {
+      "module": [
+        {
+          "@name": "$c.common",
+          "@src": "/cm/project_udc/common.xml"
+        },
+        {
+          "@name": "$c.str",
+          "@src": "/cm/project_udc/commonStr.xml"
+        },
+        {
+          "@name": "$c.num",
+          "@src": "/cm/project_udc/commonNum.xml"
+        },
+        {
+          "@name": "$c.menu",
+          "@src": "/cm/project_udc/commonMenu.xml"
+        },
+        {
+          "@name": "$c.code",
+          "@src": "/cm/project_udc/commonCode.xml"
+        },
+        {
+          "@name": "$c.popup",
+          "@src": "/cm/project_udc/commonPopup.xml"
+        },
+        {
+          "@name": "$c.util",
+          "@src": "/cm/project_udc/commonUtil.xml"
+        }
+      ]
+    },
+    "license": {
+      "@value": "true"
+    }
+  }
+}
