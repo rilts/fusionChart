6a79ea566d5f4ce3298230e01dc877ca114176d4
rilts <rialtoksj@yahoo.com>
2023-05-30 16:16:37 +0900

    환경설정

--- src/main/webapp/_wpack_/fusionChart/chart.js
+++
@@ -1,7 +0,0 @@
-/*amd /fusionChart/chart.xml 950 2a39efee39b694c5c6a20ed7b5a9733b36fda12cd65ee8635665e0eba42ef084 */
-define({declaration:{A:{version:'1.0',encoding:'UTF-8'}},E:[{T:1,N:'html',A:{xmlns:'http://www.w3.org/1999/xhtml','xmlns:ev':'http://www.w3.org/2001/xml-events','xmlns:w2':'http://www.inswave.com/websquare','xmlns:xf':'http://www.w3.org/2002/xforms'},E:[{T:1,N:'head',E:[{T:1,N:'w2:type',E:[{T:3,text:'COMPONENT'}]},{T:1,N:'w2:buildDate'},{T:1,N:'w2:MSA'},{T:1,N:'xf:model',E:[{T:1,N:'w2:dataCollection',A:{baseNode:'map'}},{T:1,N:'w2:workflowCollection'}]},{T:1,N:'w2:layoutInfo'},{T:1,N:'w2:publicInfo',A:{method:''}},{T:1,N:'script',A:{lazy:'false',type:'text/javascript'},E:[{T:4,cdata:function(scopeObj){with(scopeObj){
-scwin.onpageload = function() {
-	 
-};
-
-}}}]}]},{T:1,N:'body',A:{'ev:onpageload':'scwin.onpageload'},E:[{T:1,N:'w2:fusionchart',A:{chartType:'OverlappedBar2D',drawType:'javascript',id:'fusionchart1',labelNode:'Month',ref:'data:chartData2',seriesColumns:'[\'quarter2\']',style:'position: relative ;margin:10px;height: 300px;'}}]}]}]})
